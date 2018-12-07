24a1d489f2d8d25c837c2c37cf70d88a38aeb2b7
Geovana Martins <geovanamartins2107@gmail.com>
2018-12-07 17:03:58 -0200

    Novo Script

---
+++ scripts.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Java Script</title>
+<script>
+function funcao1()
+{
+alert("Olá");
+}
+</script>
+</head>
+<body>
+ 
+<input type="button" onclick="funcao1()" value="Exibir Alert" />
+ 
+</body>
+</html></div>
